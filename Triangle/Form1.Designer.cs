33f597d884536490503200caccb3dd1cb48effbe
Taboo_ <113307336+BadToTheBoneRiff@users.noreply.github.com>
2023-10-15 12:01:36 +0300

    Add files via upload

---
+++ Triangle/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Triangle {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.aSide = new System.Windows.Forms.TextBox();
+            this.bSide = new System.Windows.Forms.TextBox();
+            this.cSide = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.answer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCheck.Location = new System.Drawing.Point(314, 236);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(174, 55);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Проверить";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // aSide
+            // 
+            this.aSide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.aSide.Location = new System.Drawing.Point(170, 103);
+            this.aSide.Name = "aSide";
+            this.aSide.Size = new System.Drawing.Size(85, 26);
+            this.aSide.TabIndex = 1;
+            // 
+            // bSide
+            // 
+            this.bSide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bSide.Location = new System.Drawing.Point(360, 103);
+            this.bSide.Name = "bSide";
+            this.bSide.Size = new System.Drawing.Size(85, 26);
+            this.bSide.TabIndex = 2;
+            // 
+            // cSide
+            // 
+            this.cSide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cSide.Location = new System.Drawing.Point(557, 103);
+            this.cSide.Name = "cSide";
+            this.cSide.Size = new System.Drawing.Size(85, 26);
+            this.cSide.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(166, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Сторона A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(356, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Сторона B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(553, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Сторона C";
+            // 
+            // answer
+            // 
+            this.answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.answer.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.answer.Location = new System.Drawing.Point(272, 308);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(258, 44);
+            this.answer.TabIndex = 7;
+            this.answer.Text = "Введите стороны треугольника";
+            this.answer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 441);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cSide);
+            this.Controls.Add(this.bSide);
+            this.Controls.Add(this.aSide);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox aSide;
+        private System.Windows.Forms.TextBox bSide;
+        private System.Windows.Forms.TextBox cSide;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label answer;
+    }
+}
+
---
+++ Triangle/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triangle 
+{
+    public partial class Form1 : Form 
+    {
+        public Form1() 
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e) 
+        {
+            int a, b, c;
+
+            if(int.TryParse(aSide.Text, out a) && (int.TryParse(bSide.Text, out b) && (int.TryParse(cSide.Text, out c))))
+            {
+                if(a == b && b == c) 
+                {
+                    answer.Text = "Равносторонний";
+                }
+                else if (a == b || b == c || a == c)
+                {
+                    answer.Text = "Равнобедренный";
+                }
+                else
+                {
+                    answer.Text = "Разносторонний";
+                }
+            }
+            else
+            {
+                answer.Text = "Введите стороны треугольника";
+            }
+        }
+    }
+}
